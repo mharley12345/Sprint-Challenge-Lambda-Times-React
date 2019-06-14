cd054780f0882280548a393b7c38fdb732d1b5a0
mharley12345 <mharely12345@gmail.com>
2019-06-14 09:28:17 -0700

    Tab and Card Content Rendered, Start styling Header And TopBar Component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,7 +6,16 @@ const Tabs = props => {
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+          props.tabs.map(tab => {
+            return <Tab
+                      tab={tab}
+                      key={Math.floor(Math.random() * 10000) + 1}
+                      selectedTabHandler={props.selectedTabHandler}
+                      selectedTab={props.selected}
+                    />
+          })
+        }
       </div>
     </div>
   );
